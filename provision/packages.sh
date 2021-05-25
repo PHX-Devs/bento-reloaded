6f0eaece6bb3c81b0b79b3fab5a2bd5ba073a508
John Zechlin <johnzech@gmail.com>
2021-05-25 11:12:31 -0500

    add templated html hello world module (using fast api)

--- provision/packages.sh
+++ provision/packages.sh
@@ -11,6 +11,7 @@ pip3.8 install fastapi
 pip3.8 install hypercorn
 pip3.8 install sqlalchemy
 pip3.8 install psycopg2-binary
+pip3.8 install jinja2
 
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 dnf -qy module disable postgresql
